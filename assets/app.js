b8d931664997811777e2d887693de3abafa74a2e
Gavesh Bimsath <91552385+gbimsath@users.noreply.github.com>
2022-08-19 20:29:12 +0530

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,52 @@
+const image = document.querySelector('.image');
+const hover = document.querySelector('.hover');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close');
+
+function show() {
+     hover.classList.add('active');
+     modal.classList.add('show');
+}
+
+function hide() {
+     hover.classList.remove('active');
+     modal.classList.remove('show');
+}
+
+image.addEventListener('click', show);
+close.addEventListener('click', hide);
+
+var chkbtn = document.getElementById("chkbtn");
+const sidebar = document.getElementById('sidebar');
+
+function toogle() {
+     var chk = document.getElementById('check').checked;
+     if (!chk) {
+          sidebar.style.display = 'block';
+     } else {
+          sidebar.style.display = 'none';
+     }
+}
+
+chkbtn.addEventListener('click', toogle);
+
+const container = document.querySelector('#body');
+const toggle = document.querySelector('.toggle-input');
+const initialState = 'false';
+toggle.checked = initialState;
+
+toggle.addEventListener('change', function() {
+  if (!toggle.checked) {
+       container.className = 'light';
+  }
+  else {
+       container.className = 'dark';
+ }
+});
+
+particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
+
+document.addEventListener("contextmenu", function(event){
+     event.preventDefault();
+     alert("This is Gavesh Bimsath's Website");    
+     }, true);
